aa48bab9fe38b4d76314b5a2a4c680bacf99a0ba
julcarcos <juliancarrascosacosano@gmail.com>
2025-04-09 21:58:55 +0200

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HistoriaComponent } from './historia/historia.component';
 import { DetallesComponent } from './detalles/detalles.component';
 import { GaleriaComponent } from './galeria/galeria.component';
 import { RsvpComponent } from './rsvp/rsvp.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HistoriaComponent, DetallesComponent, GaleriaComponent, RsvpComponent],
+  imports: [HeaderComponent, HistoriaComponent, DetallesComponent, GaleriaComponent, RsvpComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
